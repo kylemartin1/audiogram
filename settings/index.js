0577305eeb204b12191d7dfc30079f01cf7a86cb
kylemartin1 <kylemartin1@users.noreply.github.com>
2016-09-16 17:02:46 -0700

    Update index.js

--- settings/index.js
+++ settings/index.js
@@ -19,8 +19,8 @@
 var path = require("path");
 
 module.exports = {
-  workingDirectory: path.join(__dirname, "../../static/audiogram/", "tmp"),
-  storagePath: path.join(__dirname, "../../static/audiogram/", "media"),
+  workingDirectory: path.join(__dirname, "App/project/static/audiogram/", "tmp"),
+  storagePath: path.join(__dirname, "App/project/static/audiogram/", "media"),
   fonts: [
     { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Regular.ttf") },
     { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Light.ttf"), weight: 300 },
