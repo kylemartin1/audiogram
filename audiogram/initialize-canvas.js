7333c472e77727e87816a623a3582f99c96654e8
kylemartin1 <kylemartin1@users.noreply.github.com>
2016-10-14 15:46:57 -0700

    Canvas now use generic image

--- audiogram/initialize-canvas.js
+++ audiogram/initialize-canvas.js
@@ -1,31 +1,29 @@
 var fs = require("fs"),
     path = require("path"),
     Canvas = require("canvas"),
-    getRenderer = require("../renderer/");
+    getRenderer = require("../renderer/"),
+    serverSettings = require("../settings/");
 
-function initializeCanvas(theme, cb) {
+function initializeCanvas(options, cb) {
 
   // Fonts pre-registered in bin/worker
-  var renderer = getRenderer(theme);
 
-  if (!theme.backgroundImage) {
+  var canvas = new Canvas(options.width, options.height),
+      context = canvas.getContext("2d"),
+      renderer = getRenderer(context).update(options);
+
+  renderer.caption = options.caption;
+
+  if (!options.backgroundImage) {
     return cb(null, renderer);
   }
 
-  // Load background image from file (done separately so renderer code can work in browser too)
-  fs.readFile(path.join(__dirname, "..", "settings", "backgrounds", theme.backgroundImage), function(err, raw){
+  var bg = new Image();
+  bg.src = options.backgroundImage;
+  renderer.backgroundImage = bg;
+  debugger;
 
-    if (err) {
-      return cb(err);
-    }
-
-    var bg = new Canvas.Image;
-    bg.src = raw;
-    renderer.backgroundImage(bg);
-
-    return cb(null, renderer);
-
-  });
+  return cb(null, renderer);
 
 }
 
