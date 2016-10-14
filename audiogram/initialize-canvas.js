325a666976f8b60e18598ffb90c7122c8a43bd9b
kylemartin1 <kylemartin1@users.noreply.github.com>
2016-10-14 16:26:39 -0700

    Should work now

--- audiogram/initialize-canvas.js
+++ audiogram/initialize-canvas.js
@@ -1,29 +1,22 @@
 var fs = require("fs"),
     path = require("path"),
     Canvas = require("canvas"),
-    getRenderer = require("../renderer/"),
-    serverSettings = require("../settings/");
+    getRenderer = require("../renderer/");
 
-function initializeCanvas(options, cb) {
+function initializeCanvas(theme, cb) {
 
   // Fonts pre-registered in bin/worker
+  var renderer = getRenderer(theme);
 
-  var canvas = new Canvas(options.width, options.height),
-      context = canvas.getContext("2d"),
-      renderer = getRenderer(context).update(options);
-
-  renderer.caption = options.caption;
-
-  if (!options.backgroundImage) {
+  if (!theme.backgroundImage) {
     return cb(null, renderer);
   }
 
-  var bg = new Image();
-  bg.src = options.backgroundImage;
-  renderer.backgroundImage = bg;
-  debugger;
-
-  return cb(null, renderer);
+    var bg = new Canvas.Image;
+    bg.src = theme.backgroundImage;
+    renderer.backgroundImage(bg);
+    
+    return cb(null, renderer);
 
 }
 
